6bf98b5a07045bd4c069386cb8dd600b7b27e53d
Rahul <>
2016-12-05 23:34:04 +0530

    Square of values without multiplication and stack with minimum value support

---
+++ src/main/java/com/rahul/problem/practice/math/PartitionForZeroSum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rahul.problem.practice.math;
+
+/**
+ * Given an array of numbers one to n, use + and - sign such that the returned sum is zero.
+ * 
+ * @author Rahul
+ */
+public class PartitionForZeroSum {
+    
+}
---
+++ src/main/java/com/rahul/problem/practice/math/SquareWithoutMultiplicationFromSeries.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rahul.problem.practice.math;
+
+/**
+ * Given two series : - 
+ * First : square of 1 .. n numbers
+ * Second ; Difference between those square values
+ * 
+ * Using such series devise a method to find square of any number without using multiplication.
+ * 
+ * Solution: 
+ * If we look at such series:
+ * 1, 4, 9, 16, 25, ....
+ * 3, 5, 7, 9,..
+ * Then square(5) = square(5 - 1) + (5 - 1) * 2 + 1
+ * 
+ * So, square(n) = square(n - 1) + (n - 1) * 2 + 1
+ * 
+ * NOTE: To multiply by 2, i.e. to double any number, we left shift it by 1.
+ * 
+ * @author Rahul
+ */
+public class SquareWithoutMultiplicationFromSeries {
+    
+    public int square(int _n) {
+        if (_n == 1) {
+            return 1;
+        }
+        
+        return square(_n - 1) + ((_n - 1) << 1) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SquareWithoutMultiplicationFromSeries().square(6));
+    }
+}
